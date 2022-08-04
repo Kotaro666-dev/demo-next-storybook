aa9880769c2c651499b9a00519088e05788bd83c
Kotaro Kamashima <kotaro0726@gmail.com>
2022-08-05 00:37:36 +0900

    Update: Add sample button

---
+++ component/buttons/BasicButton.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+enum ButtonSize {
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "LARGE",
+}
+
+type Props = {
+  isOutlined?: boolean;
+  size?: ButtonSize;
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+export const Button: React.FC<Props> = ({
+  isOutlined = false,
+  size = ButtonSize.MEDIUM,
+  children,
+  onClick,
+}) => {
+  return (
+    <button
+      type="button"
+      className={`
+        rounded
+        ${size === ButtonSize.MEDIUM ? "px-5 py-1" : "px-3 py-1 text-sm"}
+        ${
+          isOutlined
+            ? "border border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white"
+            : "border-none bg-blue-600 text-white hover:bg-blue-500"
+        }
+      `}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
